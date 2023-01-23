8351677bdfbaf1432a9d70eb3b072d894a1e9cdb
denisplay <workingdenniss@gmail.com>
2023-01-23 20:25:24 +0200

    no more .clone() now using a reference string
    and created a big func to find  the crf needed for future use

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{process::Command, fs::{File, self}, io::Write};
 use serde_json::Value;
 use clap::Parser;
-
 use std::time::Instant;
 
 #[derive(Parser)]
@@ -37,6 +36,10 @@ struct Args {
     /// Clip Interval in seconds
     #[arg(short = 'n', long, default_value_t = 360)]//every 6 min
     clip_interval: i32,
+
+    /// select what crf to use on output video (average/smallest)
+    #[arg(short = 'u', long, default_value_t = String::from("smallest"))]
+    crf_option: String,
  }
 
 fn main() {
@@ -52,6 +55,7 @@ fn main() {
     let mut current_crf = args.crf;
     let clip_length = args.clip_length;
     let clip_interval = args.clip_interval;
+    let crf_used = args.crf_option;
 
     check_and_create_folders_helpers();
 
@@ -73,7 +77,7 @@ fn main() {
 
 
     //get clips
-    let clip_names = extract_clips(input_file.clone(), clip_length, clip_interval, ffmpeg_path, ffprobe_path).unwrap();
+    let clip_names = extract_clips(&input_file, clip_length, clip_interval, &ffmpeg_path, &ffprobe_path).unwrap();
 
     if clip_names[0] == input_file{
         println!("Clip_Length is bigger then the whole video, please check the settings");
@@ -82,67 +86,48 @@ fn main() {
         return;
     }
 
-    let ssmi2_check_valid = false;
+    let _num_of_clips = clip_names.len();
     let mut crf_values: Vec<i32> = Vec::new();
     //for each clip find crf
-    for i in 0..clip_names.len(){
-        let current_clip_name = format!("output_helper/clips/{}", clip_names[i]);
-        let current_clip_encoded_name = format!("output_helper/clips_encoded/{}",clip_names[i]);
-        let mut was_above_90 = false;
-        let mut was_below_90 = false;
-        while !ssmi2_check_valid {
-            let current_crf_str: String = current_crf.to_string();
-            let av1an_settings = format_encoding_settings(av1an_setings_unformatted.clone(), current_clip_name.clone(), speed.clone(), current_crf_str.clone(), worker_num.clone(), current_clip_encoded_name.clone());
-            encode_clip(current_clip_name.clone(), av1an_path.clone(), av1an_settings).unwrap();
-            let ssim2_results = ssim2_clip(current_clip_name.clone(), current_clip_encoded_name.clone(), arch_path.clone(), ssim2_path.clone(), worker_num.to_string()).unwrap();
-            let result_95: i32 = ssim2_results[0].parse().unwrap();
-            println!("\n\n\n\ncurrent_clip: {}, current_crf: {}, current_ssim2: {}", current_clip_name.clone(), current_crf, ssim2_results[0]);
-            if result_95 == 90{
-                //save current crf for later use
-                crf_values.push(current_crf);
-                break;//found the crf wanted
-            }
-            if result_95 < 90{
-                if was_above_90{
-                    was_below_90 = true;
-                    current_crf -= 1;
-                }
-                else{
-                    current_crf -= 5;
-                }
-            }
-            if result_95 > 90{
-                was_above_90 = true;
-                if was_below_90{
-                    current_crf += 1;
-                }
-                else{
-                    current_crf += 5;
-                }
-            }
-            fs::remove_file(current_clip_encoded_name.clone()).unwrap();//delete encoded file to encode again
-            fs::remove_file(current_clip_encoded_name.clone() + &".lwi".to_string()).unwrap();//delete encoded file iwi for ssim2
-        }
+    for i in 0..clip_names.len() {
+        let crf_90 = find_crf_for_90_ssim2(current_crf, 
+            &clip_names[i], 
+            &av1an_setings_unformatted, 
+            &speed, 
+            &worker_num, 
+            &av1an_path, 
+            &arch_path, 
+            &ssim2_path
+        );
+        current_crf = crf_90;
+        crf_values.push(crf_90);
     }
     let min_crf = find_lowest_crf(crf_values.clone());
-    let average_crf = find_average_crf(crf_values);
+    let average_crf = find_average_crf(crf_values.clone());
+    let crf_used_use;
+    if crf_used == "smallest".to_string(){
+        crf_used_use = min_crf;
+    }
+    else{
+        crf_used_use = average_crf;
+    }
+    println!("{:?}", crf_values);
     println!("min_crf: {}", min_crf);
     println!("average_crf: {}", average_crf);
     println!("TIME_ELAPSED: {}", now.elapsed().as_secs());
-    let current_crf_str: String = current_crf.to_string();
-    let av1an_settings = format_encoding_settings(av1an_setings_unformatted.clone(), input_file.clone(), speed.clone(), current_crf_str.clone(), worker_num.clone(), output_file.clone());
-    encode_clip(input_file.clone(), av1an_path.clone(), av1an_settings).unwrap();
+    let av1an_settings = format_encoding_settings(&av1an_setings_unformatted, &input_file, &speed, &crf_used_use.to_string(), &worker_num, &output_file);
+    encode_clip(&input_file, &av1an_path, &av1an_settings).unwrap();
     println!("Finished Encoding: {}", input_file);
 
 }
 
-fn encode_clip(clip_path: String, av1an_path: String, av1an_settings: String) -> Result<i32, String>{
+fn encode_clip(clip_path: &String, av1an_path: &String, av1an_settings: &String) -> Result<i32, String>{
     //
     //  start encoding a clip with crf given and additional settings
     //
     let file_name = "av1an_encode_settings.bat".to_string();
     //try to create a file to encode with
-    match create_file_encoding_settings(av1an_settings.clone(), file_name.clone()){
+    match create_file_encoding_settings(&av1an_settings, &file_name){
         Ok(ok) => ok,
         Err(_err) => {
             let error_messege = "Cannot Create File".to_string();
@@ -152,11 +137,11 @@ fn encode_clip(clip_path: String, av1an_path: String, av1an_settings: String) ->
     //try start encoding
     let av1an_args = ["/C", &file_name, &av1an_path];
     let av1an_error = format!("Cannot start encoding file: {}\nError: ", clip_path);
-    spawn_a_process(av1an_args, av1an_error).unwrap();
+    spawn_a_process(av1an_args, &av1an_error).unwrap();
     return Ok(0);
 }
 
-fn ssim2_clip(original_clip_path: String, encoded_clip_path: String, arch_path: String, ssim2_path: String, worker_num: String) -> Result<Vec<String>, String>{
+fn ssim2_clip(original_clip_path: &String, encoded_clip_path: &String, arch_path: &String, ssim2_path: &String, worker_num: &String) -> Result<Vec<String>, String>{
     //run ssmi2 with arch wsl
     //return 95th percentile and 5th percentile if succeeded
     let mut results_vec: Vec<String> = Vec::new();
@@ -167,7 +152,7 @@ fn ssim2_clip(original_clip_path: String, encoded_clip_path: String, arch_path: 
         ssim2_path, worker_num, original_clip_path, encoded_clip_path, save_file_name);
 
     let file_name = "ssmi2_encode_settings.bat".to_string();
-    match create_file_encoding_settings(ssmi2_settings, file_name.clone()){
+    match create_file_encoding_settings(&ssmi2_settings, &file_name){
         Ok(ok) => ok,
         Err(_err) => {
             let error_messege = "Cannot Create File".to_string();
@@ -178,7 +163,7 @@ fn ssim2_clip(original_clip_path: String, encoded_clip_path: String, arch_path: 
     //try start ssim2
     let ssim2_args = ["/C", &file_name, &arch_path];
     let ssim2_error = format!("While Trying to ssim2 clip: {}\nError: ", encoded_clip_path);
-    spawn_a_process(ssim2_args, ssim2_error).unwrap();
+    spawn_a_process(ssim2_args, &ssim2_error).unwrap();
 
     let output_file_content = fs::read_to_string(save_file_name).expect("Should have been able to read ssim2_output.txt");
     let lines: Vec<&str> = output_file_content.split("\n").collect();
@@ -192,7 +177,7 @@ fn ssim2_clip(original_clip_path: String, encoded_clip_path: String, arch_path: 
     return Ok(results_vec);
 }
 
-fn create_file_encoding_settings(settings: String, file_name: String) -> Result<String, i32>{
+fn create_file_encoding_settings(settings: &String, file_name: &String) -> Result<String, i32>{
     //
     //  writes a batch file to encode with later
     //  this is becuase procces in rust use string leterals or something :(
@@ -281,7 +266,7 @@ fn get_json() -> Result<Vec<String>, String>{
     return Ok(final_vec);
 }
 
-fn extract_clips(full_video: String, clip_length: i32, interval: i32, ffmpeg_path: String, ffprobe_path: String) -> Result<Vec<String>, String>{
+fn extract_clips(full_video: &String, clip_length: i32, interval: i32, ffmpeg_path: &String, ffprobe_path: &String) -> Result<Vec<String>, String>{
     //
     //  first get the video length using ffprobe
     //  then in a for loop extract each clip using the clip_length and the interval
@@ -297,7 +282,7 @@ fn extract_clips(full_video: String, clip_length: i32, interval: i32, ffmpeg_pat
         full_video, file_name_ffprobe_output);
     
     //try to create a file to encode with
-    match create_file_encoding_settings(ffprobe_settings.clone(), file_name_ffprobe.clone()){
+    match create_file_encoding_settings(&ffprobe_settings, &file_name_ffprobe){
         Ok(ok) => ok,
         Err(_err) => {
             let error_messege = "Cannot Create File".to_string();
@@ -306,7 +291,7 @@ fn extract_clips(full_video: String, clip_length: i32, interval: i32, ffmpeg_pat
     };
     let ffprobe_args = ["/C", &file_name_ffprobe, &ffprobe_path];
     let ffprobe_error = format!("Cannot probe file: {}\nError: ", full_video);
-    output_a_process(ffprobe_args, ffprobe_error).unwrap();
+    output_a_process(ffprobe_args, &ffprobe_error).unwrap();
 
     //read the result that was saved to a file
     let output_file_content = fs::read_to_string(file_name_ffprobe_output).expect("Should have been able to read ffprobe_output.txt");
@@ -316,7 +301,7 @@ fn extract_clips(full_video: String, clip_length: i32, interval: i32, ffmpeg_pat
     if video_length < clip_length{
         //dont make clip just tell the av1an to encode the whole video
         //as it is really small, smaller then the clip that the user wanted
-        final_vec.push(full_video);
+        final_vec.push(full_video.to_string());
         return Ok(final_vec);
     }
     let mut length_passed = 0;
@@ -326,7 +311,7 @@ fn extract_clips(full_video: String, clip_length: i32, interval: i32, ffmpeg_pat
         let ffmpeg_settings = format!("%1 -ss {} -i \"{}\" -c copy -t {} \"output_helper/clips/{}\"",
             length_passed, full_video, clip_length, current_file_name);
 
-        match create_file_encoding_settings(ffmpeg_settings.clone(), file_name_ffmpeg.clone()){
+        match create_file_encoding_settings(&ffmpeg_settings, &file_name_ffmpeg){
             Ok(ok) => ok,
             Err(_err) => {
                 let error_messege = "Cannot Create File".to_string();
@@ -336,7 +321,7 @@ fn extract_clips(full_video: String, clip_length: i32, interval: i32, ffmpeg_pat
 
         let ffmpeg_args = ["/C", &file_name_ffmpeg, &ffmpeg_path];
         let ffmpeg_error = format!("Cannot clip file: {}\nError: ", full_video);
-        output_a_process(ffmpeg_args, ffmpeg_error).unwrap();
+        output_a_process(ffmpeg_args, &ffmpeg_error).unwrap();
         final_vec.push(current_file_name);
 
         length_passed += clip_length + interval;
@@ -346,7 +331,7 @@ fn extract_clips(full_video: String, clip_length: i32, interval: i32, ffmpeg_pat
     return Ok(final_vec);
 }
 
-fn format_encoding_settings(settings: String, input_file: String, speed: String, crf: String, worker_num: String, output_file: String) -> String{
+fn format_encoding_settings(settings: &String, input_file: &String, speed: &String, crf: &String, worker_num: &String, output_file: &String) -> String{
     let mut final_string = settings.clone();
     final_string = final_string.replace("INPUT", &("\"".to_string() + &input_file + &"\"".to_string()));//INPUT
     final_string = final_string.replace("SPEED", &speed);//SPEED
@@ -358,45 +343,90 @@ fn format_encoding_settings(settings: String, input_file: String, speed: String,
 }
 
 fn check_and_create_folders_helpers(){
+    //delete latest encode
+    fs::remove_dir_all("output_helper/").unwrap();
     fs::create_dir_all("output_helper/ssim2").unwrap();
     fs::create_dir_all("output_helper/clips").unwrap();
     fs::create_dir_all("output_helper/clips_encoded").unwrap();
     fs::create_dir_all("output_helper/ffprobe").unwrap();
 }
 
-fn spawn_a_process(args: [&str; 3], custom_error: String) -> Result<i32, String>{
+fn spawn_a_process(args: [&str; 3], custom_error: &String) -> Result<i32, String>{
     //using spawn to show the user the program running
     let process = match Command::new("cmd").args(args).spawn(){
         Ok(out) => out,
-        Err(err) => return Err(custom_error + &err.to_string())
+        Err(err) => return Err(custom_error.to_string() + &err.to_string())
     };
 
     let output = match process.wait_with_output() {
         Ok(ok) => ok,
-        Err(err) => return Err(custom_error + &err.to_string())
+        Err(err) => return Err(custom_error.to_string() + &err.to_string())
     };
 
     if output.status.success(){
         return Ok(1);        
     }
     else{
-        return Err(custom_error + &"no error, some program start and finished without doing anything".to_string());
+        return Err(custom_error.to_string() + &"no error, some program start and finished without doing anything".to_string());
     }
 }
 
-fn output_a_process(args: [&str; 3], custom_error: String) -> Result<i32, String>{
+fn output_a_process(args: [&str; 3], custom_error: &String) -> Result<i32, String>{
     //using output to hide the program
     let process = match Command::new("cmd").args(args).output(){
         Ok(out) => out,
-        Err(err) => return Err(custom_error + &err.to_string())
+        Err(err) => return Err(custom_error.to_string() + &err.to_string())
     };
 
     if process.status.success(){
-        return Ok(1);  
+        return Ok(1);
     }
     else{
-        return Err(custom_error + &"no error, some program start and finished without doing anything".to_string());
+        return Err(custom_error.to_string() + &"no error, some program start and finished without doing anything".to_string());
     }
+}
+
+fn find_crf_for_90_ssim2(starting_crf: i32, clip_name: &String, av1an_setings_unformatted: &String, speed: &String, worker_num: &String, av1an_path: &String, arch_path: &String, ssim2_path: &String) -> i32{
+    let mut current_crf = starting_crf;
+    let ssmi2_check_valid = false;
+    let current_clip_name = format!("output_helper/clips/{}", clip_name);
+    let current_clip_encoded_name = format!("output_helper/clips_encoded/{}",clip_name);
+    let mut was_above_90 = false;
+    let mut was_below_90 = false;
+    while !ssmi2_check_valid {
+        let current_crf_str: String = current_crf.to_string();
+        let av1an_settings = format_encoding_settings(av1an_setings_unformatted, &current_clip_name, speed, &current_crf_str, worker_num, &current_clip_encoded_name);
+        encode_clip(&current_clip_name, av1an_path, &av1an_settings).unwrap();
+        let ssim2_results = ssim2_clip(&current_clip_name, &current_clip_encoded_name, arch_path, ssim2_path, &worker_num.to_string()).unwrap();
+        let result_95: i32 = ssim2_results[0].parse().unwrap();
+        println!("\n\n\n\ncurrent_clip: {}, current_crf: {}, current_ssim2: {}", current_clip_name, current_crf, ssim2_results[0]);
+        if result_95 == 90{
+            return current_crf;
+            //found the crf wanted
+        }
+        if result_95 < 90{
+            if was_above_90{
+                was_below_90 = true;
+                current_crf -= 1;
+            }
+            else{
+                current_crf -= 5;
+            }
+        }
+        if result_95 > 90{
+            was_above_90 = true;
+            if was_below_90{
+                current_crf += 1;
+            }
+            else{
+                current_crf += 5;
+            }
+        }
+        fs::remove_file(current_clip_encoded_name.clone()).unwrap();//delete encoded file to encode again
+        fs::remove_file(current_clip_encoded_name.clone() + &".lwi".to_string()).unwrap();//delete encoded file iwi for ssim2
+    }
+
+    return -1;
 }
 
 fn find_lowest_crf(crf_list: Vec<i32>) -> i32{
